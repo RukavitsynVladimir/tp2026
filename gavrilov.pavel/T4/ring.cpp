687e1ab549609cc35d514ff60fd8cabd6e0ad903
Pavel <pavelgavrilov812@gmail.com>
2026-03-10 17:40:38 +0300

    Create ring.cpp

---
+++ gavrilov.pavel/T4/ring.cpp
@@ -0,0 +1,35 @@
+#include "ring.h"
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+Ring::Ring(const Point& center, double outerRadius, double innerRadius)
+    : center(center), outerRadius(outerRadius), innerRadius(innerRadius) {
+    if (outerRadius <= innerRadius) {
+        outerRadius = innerRadius + 1.0; 
+    }
+}
+
+double Ring::getArea() const {
+    return M_PI * (outerRadius * outerRadius - innerRadius * innerRadius);
+}
+
+Point Ring::getCenter() const {
+    return center;
+}
+
+void Ring::move(double dx, double dy) {
+    center.x += dx;
+    center.y += dy;
+}
+
+void Ring::scale(double factor) {
+    outerRadius *= factor;
+    innerRadius *= factor;
+}
+
+std::string Ring::getName() const {
+    return "RING";
+}
