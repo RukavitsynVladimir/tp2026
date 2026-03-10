53a0c9509ac2e35344eee8278d5f6535cca6ced8
vladimir <vovaah021007@gmail.com>
2026-03-10 23:41:23 +0300

    fixed build x5

--- khaisarov.vladimir/T4/compositeshape.h
+++ khaisarov.vladimir/T4/compositeshape.h
@@ -8,18 +8,18 @@
 class CompositeShape : public Shape
 {
 private:
-    std::vector<std::unique_ptr<Shape>> shapes;
+  std::vector<std::unique_ptr<Shape>> shapes;
 public:
-    CompositeShape() = default;
-    CompositeShape(std::vector<std::unique_ptr<Shape>>&& s);
-    double getArea() const override;
-    Point getCenter() const override;
-    std::string getName() const override;
-    void move(double dx, double dy) override;
-    void scale(double factor) override;
-    std::pair<Point, Point> Bounds() const override;
-    void print(std::ostream& os) const;
-    void add(std::unique_ptr<Shape> shape);
+  CompositeShape() = default;
+  CompositeShape(std::vector<std::unique_ptr<Shape>>&& s);
+  double getArea() const override;
+  Point getCenter() const override;
+  std::string getName() const override;
+  void move(double dx, double dy) override;
+  void scale(double factor) override;
+  std::pair<Point, Point> Bounds() const override;
+  void print(std::ostream& os) const;
+  void add(std::unique_ptr<Shape> shape);
 };
 
 #endif
